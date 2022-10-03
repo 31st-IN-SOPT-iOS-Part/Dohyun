bfcbc07cb55448cfbef568b6dcb1d8df633e6187
Dohyun Chung <pastapeter@naver.com>
2022-10-03 17:09:18 +0900

    make project

---
+++ 1st_Seminar_Assignment/1st_Seminar_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1st_Seminar_Assignment
+//
+//  Created by Jung peter on 10/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
